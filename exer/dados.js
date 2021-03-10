7387a20159c9311d8355a67638b2a794fd746ceb
FelipeConsorte2001 <79878849+FelipeConsorte2001@users.noreply.github.com>
2021-03-10 15:30:08 -0300

    Add files via upload

---
+++ exer/dados.js
@@ -0,0 +1,52 @@
+let dados =[
+    {
+        "titulo": "Java",
+        "autores" : "Matheus e Kauan",
+        "edição":"especial",
+        "ano":"2020",
+        "editora":"Programa",
+        "classificacao":"Primeiro",
+        "codigo":"01",
+        "assunto":"Programação"
+    },
+    {
+        "titulo": "Python",
+        "autores" : "Sicrano ",
+        "edição":"Comercial",
+        "ano": "2020",
+        "editora":"Tecnologia",
+        "classificacao":"Terceira",
+        "codigo":"02",
+        "assunto":"Programação"
+    },
+    {
+        "titulo": "Javascript",
+        "autores" : "Jorge e Matheus ",
+        "edição":"Limitada",
+        "ano":"2020",
+        "editora":"Tecnologia",
+        "classificacao":"Segunda",
+        "codigo":"03",
+        "assunto":"Programação"
+    },
+    {
+        "titulo": "Cobol",
+        "autores" : "Luis",
+        "edição":"especial",
+        "ano":"2010",
+        "editora":"Programa",
+        "classificacao":"Quinto",
+        "codigo":"04",
+        "assunto":"Programação"
+    },
+    {
+        "titulo": "Angular",
+        "autores" : "Felipe",
+        "edição":"especial",
+        "ano":"2015",
+        "editora":"Progrma bem",
+        "classificacao":"Setimo",
+        "codigo":"05",
+        "assunto":"Framework"
+    }
+];
